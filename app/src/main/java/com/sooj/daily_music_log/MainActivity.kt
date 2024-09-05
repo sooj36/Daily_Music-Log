c0641779c55edbb00207a2f5a2999383c8c1771c
sooj36 <sooj2023@gmail.com>
2024-09-05 20:42:53 +0900

    [feat] 네비게이션 설정

--- app/src/main/java/com/sooj/daily_music_log/MainActivity.kt
+++ app/src/main/java/com/sooj/daily_music_log/MainActivity.kt
@@ -6,11 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.sooj.daily_music_log.presentation.MainPageScreen
 import com.sooj.daily_music_log.ui.theme.DailyMusicLogTheme
+import androidx.navigation.compose.composable
+import com.sooj.daily_music_log.presentation.DetailPageScreen
+import com.sooj.daily_music_log.presentation.EditDetailPageScreen
+import com.sooj.daily_music_log.presentation.PosterListScreen
+import com.sooj.daily_music_log.presentation.SearchPageScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +28,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Screen.MainPage.route) {
+                        composable(Screen.MainPage.route) { MainPageScreen(navController) }
+                        composable(Screen.SearchPage.route) { SearchPageScreen()}
+                        composable(Screen.PosterList.route) { PosterListScreen(navController) }
+                        composable(Screen.DetailPage.route) { DetailPageScreen() }
+                        composable(Screen.EditDetailPage.route) { EditDetailPageScreen() }
 
+                    }
                 }
             }
         }
