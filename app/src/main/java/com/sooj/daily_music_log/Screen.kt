88e9ecf2371379c9e7b34964c3cafcdce0df24c7
sooj36 <sooj2023@gmail.com>
2024-09-05 20:37:03 +0900

    [setting] screen , enum class 구현

---
+++ app/src/main/java/com/sooj/daily_music_log/Screen.kt
@@ -0,0 +1,9 @@
+package com.sooj.daily_music_log
+
+enum class Screen (val route : String) {
+    MainPage("main_page"),
+    SearchPage("search_page"),
+    PosterList("poster_list"),
+    DetailPage("detail_page"),
+    EditDetailPage("edit_detail_page")
+}
