9504db99ce4e9ee864ba0d851b2d8ee313f6f8a4
sooj36 <sooj2023@gmail.com>
2024-09-05 23:21:05 +0900

    [feat] viewmodel , impl 설정

--- app/src/main/java/com/sooj/daily_music_log/presentation/viewModel/TrackViewModel.kt
+++ app/src/main/java/com/sooj/daily_music_log/presentation/viewModel/TrackViewModel.kt
@@ -38,52 +38,46 @@ class TrackViewModel @Inject constructor(
 
     fun getMusic_vm(track: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            viewModelScope.launch(Dispatchers.IO) {
-                try {
-                    val trackinfo = repository.getMusic_impl(track)
-                    withContext(Dispatchers.Main) {
-                        _searchList.value = trackinfo
-                    }
-                } catch (e : Exception) {
-                    Log.e("sj_getMusic_not", "${e.message}")
+            Log.d("sj_vm(↓) GETMUSIC", "Running on thread: ${Thread.currentThread().name}")
+            try {
+                val trackInfo = repository.getMusic_impl(track)
+                withContext(Dispatchers.Main) {
+                    _searchList.value = trackInfo
                 }
+            } catch (e : Exception) {
+                Log.e("sj getmusic vm", "ERROR FETCHING TRACK INFO ${e.message}")
             }
+            Log.d("sj_vm(↑) GETMUSIC", "Running on thread: ${Thread.currentThread().name}")
         }
     }
 
     /** 셀렉 트랙 */
     fun selectedTrack_vm(track: Track) {
-        _selectedTrack.value = track
+        _selectedTrack.value = track //사용자가 선택한 track을 전달받아 상태 갱신
 
         getPoster_vm()
     }
 
     /** 셀렉 트랙으로 get 포스터 */
     fun getPoster_vm() {
-        val selectedPosterInfo = _selectedTrack.value ?: return
-
         viewModelScope.launch(Dispatchers.IO) {
             Log.d("sj_vm(↓) GETPOSTER", "Running on thread: ${Thread.currentThread().name}")
-            val albumInfo = repository.getPoster_impl(
-                selectedPosterInfo.name ?: "트랙",
-                selectedPosterInfo.artist ?: "아티스트"
-            )
-            if (albumInfo != null) {
-                val albumImageUrl = albumInfo.image.find { it.size == "large" }?.url
+           // 일단 선택한 트랙 값 가져와야해
+            val selectTrack = _selectedTrack.value ?: return@launch
+
+            if (selectTrack != null) {
+                val ImageUrl = selectTrack.image?.find { it.size == "large" }?.url
                 withContext(Dispatchers.Main) {
-                    _getImage.value = albumImageUrl
+                    _getImage.value = ImageUrl
                     Log.d("sj_vm getposter withcontext", "Running on thread: ${Thread.currentThread().name}")
                 }
             } else {
-                Log.e("sj 포스터 에러", " 포스터 에러")
+                Log.e("앨범 정보 에러", "앨범 정보 못 가져옴 $")
             }
             Log.d("sj_vm(↑) GETPOSTER", "Running on thread: ${Thread.currentThread().name}")
         }
     }
 
     /** 데이터 불러오는 메서드 */
-    fun getAllTracks_vm() {
-
-    }
 
 }
