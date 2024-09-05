0c9fc9765588d8e365e08d5811fbcdadaba622c6
sooj36 <sooj2023@gmail.com>
2024-09-05 20:51:18 +0900

    [feat] DI 모듈 설정

---
+++ app/src/main/java/com/sooj/daily_music_log/di/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.sooj.daily_music_log.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@HiltAndroidApp
+class MyApplication : Application() {
+}
--- app/src/main/java/com/sooj/daily_music_log/presentation/SearchPage.kt
+++ app/src/main/java/com/sooj/daily_music_log/presentation/SearchPage.kt
@@ -1,8 +1,10 @@
 package com.sooj.daily_music_log.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.sooj.daily_music_log.presentation.viewModel.SearchViewModel
 
 @Composable
-fun SearchPageScreen() {
+fun SearchPageScreen(navController: NavController, musicViewModel : SearchViewModel) {
 
 }
