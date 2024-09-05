58736337d2b79af1b24ac12fe626f7c9cc783b9f
sooj36 <sooj2023@gmail.com>
2024-09-05 20:53:52 +0900

    [feat] repository 설정

---
+++ app/src/main/java/com/sooj/daily_music_log/data/TrackRepositoryImpl.kt
@@ -0,0 +1,7 @@
+package com.sooj.daily_music_log.data
+
+import com.sooj.daily_music_log.domain.TrackRepository
+import javax.inject.Inject
+
+class TrackRepositoryImpl @Inject constructor() : TrackRepository {
+}
---
+++ app/src/main/java/com/sooj/daily_music_log/domain/TrackRepository.kt
@@ -0,0 +1,4 @@
+package com.sooj.daily_music_log.domain
+
+interface TrackRepository {
+}
